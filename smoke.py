cad78bba2d74824fe3e9c72ada0b38cbd1690249
cindy-cheng0214 <80041219+cindy-cheng0214@users.noreply.github.com>
2023-05-02 11:17:36 +0800

    update

---
+++ smoke.py
@@ -0,0 +1,71 @@
+import torch
+import numpy as np
+from tqdm.auto import tqdm
+from datasets import load_dataset
+from transformers import CLIPProcessor, CLIPModel
+
+
+# Load the dataset
+test_dataset = load_dataset('/home/yuting/Smoke100k/validation/smoke100k_H',
+                            split='validation')
+
+print(test_dataset)
+print(set(test_dataset['label']))
+
+# generate sentences
+# labels = test_dataset.info.features['label'].names
+# clip_labels = [f"a photo of a {label}" for label in labels]
+clip_labels = ["an image with no smoke", "an image with smoke"]
+print(clip_labels)
+
+
+# initialization
+model_id = "openai/clip-vit-base-patch32"
+
+processor = CLIPProcessor.from_pretrained(model_id)
+model = CLIPModel.from_pretrained(model_id)
+
+# if you have CUDA set it to the active device like this
+device = "cuda" if torch.cuda.is_available() else "cpu"
+# move the model to the device
+model.to(device)
+
+# create label tokens
+label_tokens = processor(
+    text=clip_labels,
+    padding=True,
+    images=None,
+    return_tensors='pt'
+).to(device)
+
+# encode tokens to sentence embeddings
+label_emb = model.get_text_features(**label_tokens)
+# detach from pytorch gradient computation
+label_emb = label_emb.detach().cpu().numpy()
+
+# normalization
+label_emb = label_emb / np.linalg.norm(label_emb, axis=0)
+
+preds = []
+batch_size = 32
+
+for i in tqdm(range(0, len(test_dataset), batch_size)):
+    i_end = min(i + batch_size, len(test_dataset))
+    images = processor(
+        text=None,
+        images=test_dataset[i:i_end]['image'],
+        return_tensors='pt'
+    )['pixel_values'].to(device)
+    img_emb = model.get_image_features(images)
+    img_emb = img_emb.detach().cpu().numpy()
+    scores = np.dot(img_emb, label_emb.T)
+    preds.extend(np.argmax(scores, axis=1))
+
+true_preds = []
+for i, label in enumerate(test_dataset['label']):
+    if label == preds[i]:
+        true_preds.append(1)
+    else:
+        true_preds.append(0)
+
+print("Accuracy: {}" .format(sum(true_preds) / len(true_preds)))
